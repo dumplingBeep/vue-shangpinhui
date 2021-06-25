15b97e7d0a34a35d2eca78c42ae2ad4affd86096
luoxin <1813650185@qq.com>
2021-06-25 09:09:13 +0800

    Home静态页面搭建

--- src/views/Home/GridContent/index.vue
+++ src/views/Home/GridContent/index.vue
@@ -1,118 +1,123 @@
 <template>
   <div class="mainnav">
     <div class="container">
-      <!-- 侧边导航栏 -->
-      <nav class="left-sidebar">
-        <div
-          v-for="baseCategory in baseCategoryList"
-          :key="baseCategory.categoryId"
-          class="menu-item"
-        >
-          <h3 class="menu-title">
-            <router-link to="/">{{ baseCategory.categoryName }}</router-link>
-          </h3>
-          <div class="submenu-list">
-            <dl
-              v-for="categoryItem in baseCategory.categoryChild"
-              :key="categoryItem.categoryId"
-              class="submenu-item"
-            >
-              <dt class="submenu-title sm-p-l">
-                <router-link to="/">{{ categoryItem.categoryName }}</router-link>
-              </dt>
-              <dd class="link">
-                <router-link
-                  v-for="category in categoryItem.categoryChild"
-                  :key="category.categoryId"
-                  to="/"
-                >
-                  {{ category.categoryName }}
-                </router-link>
-              </dd>
-            </dl>
-          </div>
-        </div>
-      </nav>
-
       <!-- 轮播图 -->
       <div class="center-swiper">
-        <!-- Slider main container -->
-        <div
-          @mouseenter="swiper.autoplay.stop()"
-          @mouseleave="swiper.autoplay.start()"
-          class="swiper-container"
-        >
-          <!-- Additional required wrapper -->
-          <div class="swiper-wrapper">
-            <!-- Slides -->
-            <div class="swiper-slide"><img src="./banner1.jpg" alt="banner1" /></div>
-            <div class="swiper-slide"><img src="./banner2.jpg" alt="banner1" /></div>
-            <div class="swiper-slide"><img src="./banner3.jpg" alt="banner1" /></div>
-            <div class="swiper-slide"><img src="./banner4.jpg" alt="banner1" /></div>
-          </div>
-          <!-- If we need pagination -->
-          <div class="swiper-pagination"></div>
+        <Carousel :bannerList="banners" />
+      </div>
 
-          <!-- If we need navigation buttons -->
-          <div class="swiper-button-prev"></div>
-          <div class="swiper-button-next"></div>
+      <div class="right-sidebar">
+        <!-- 快报模块 -->
+        <div class="sidebar-report">
+          <p class="side-title link">
+            尚品汇快报
+            <a href="#">更多></a>
+          </p>
+          <!-- 信息列表 -->
+          <div class="report-list link">
+            <a href="#" class="report-item">
+              <span>[特惠]</span>
+              备战开学季 全民半价购数码
+            </a>
+            <a href="#" class="report-item">
+              <span>[公告]</span>
+              备战开学季 全民半价购数码
+            </a>
+            <a href="#" class="report-item">
+              <span>[特惠]</span>
+              备战开学季 全民半价购数码
+            </a>
+            <a href="#" class="report-item">
+              <span>[公告]</span>
+              备战开学季 全民半价购数码
+            </a>
+            <a href="#" class="report-item">
+              <span>[特惠]</span>
+              备战开学季 全民半价购数码
+            </a>
+          </div>
         </div>
+        <!-- 服务模块 -->
+        <div class="service-list link">
+          <a href="#" class="service-item">
+            <i class="service-item-icon"></i>
+            <p>话费</p>
+          </a>
+          <a href="#" class="service-item">
+            <i class="service-item-icon"></i>
+            <p>机票</p>
+          </a>
+          <a href="#" class="service-item">
+            <i class="service-item-icon"></i>
+            <p>电影票</p>
+          </a>
+          <a href="#" class="service-item">
+            <i class="service-item-icon"></i>
+            <p>游戏</p>
+          </a>
+          <a href="#" class="service-item">
+            <i class="service-item-icon"></i>
+            <p>彩票</p>
+          </a>
+          <a href="#" class="service-item">
+            <i class="service-item-icon"></i>
+            <p>加油站</p>
+          </a>
+          <a href="#" class="service-item">
+            <i class="service-item-icon"></i>
+            <p>酒店</p>
+          </a>
+          <a href="#" class="service-item">
+            <i class="service-item-icon"></i>
+            <p>火车票</p>
+          </a>
+          <a href="#" class="service-item">
+            <i class="service-item-icon"></i>
+            <p>众筹</p>
+          </a>
+          <a href="#" class="service-item">
+            <i class="service-item-icon"></i>
+            <p>理财</p>
+          </a>
+          <a href="#" class="service-item">
+            <i class="service-item-icon"></i>
+            <p>礼品卡</p>
+          </a>
+          <a href="#" class="service-item">
+            <i class="service-item-icon"></i>
+            <p>白条</p>
+          </a>
+        </div>
+        <!-- 广告图 -->
+        <img class="sidebar-banner" src="./ad1.png" alt="" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { reqSendBaseCategory } from './../../../api/home';
-import Swiper from 'swiper';
-import 'swiper/css/swiper.css';
+import Carousel from './../../../components/Carousel';
+import { reqGetBanners } from './../../../api/home';
 
 export default {
   name: 'GridContent',
   data() {
     return {
       baseCategoryList: [],
+      banners: [],
     };
   },
-  async mounted() {
-    // 获取左侧边导航栏数据(发送请求)
-    try {
-      const reqsponse = await reqSendBaseCategory();
-
-      // 成功
-      if (reqsponse.data.code === 200) {
-        this.baseCategoryList = reqsponse.data.data;
-      }
-    } catch (error) {
-      console.log(error);
-    }
-
-    this.swiper = new Swiper('.swiper-container', {
-      autoplay: {
-        delay: 2000,
-        disableOnInteraction: false,
-      },
-      loop: true, // 循环模式选项
-
-      // 如果需要分页器
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-
-      // 如果需要前进后退按钮
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-
-    //鼠标滑过pagination控制swiper切换
-    for (let i = 0; i < this.swiper.pagination.bullets.length; i++) {
-      this.swiper.pagination.bullets[i].onmouseover = function () {
-        this.click();
-      };
-    }
+  components: {
+    Carousel,
+  },
+  mounted() {
+    reqGetBanners()
+      .then((res) => {
+        this.banners = res;
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   },
 };
 </script>
@@ -125,78 +130,136 @@ export default {
   }
 }
 
-.left-sidebar {
-  width: 210px;
-  height: 459px;
-  background-color: #fafafa;
-  position: relative;
-  z-index: 99;
-
-  a:hover {
-    text-decoration: none;
-  }
-}
-
-.menu-item {
-  &:hover .submenu-list {
-    display: block;
-  }
-}
-
-.menu-title {
-  height: 30px;
-  line-height: 30px;
-  padding: 0 20px;
-  font-size: 14px;
-  cursor: pointer;
-
-  &:hover {
-    background: rgb(231, 231, 231);
-  }
-}
-
-.submenu-list {
-  display: none;
-  position: absolute;
-  left: 210px;
-  top: 0;
-  width: 734px;
-  min-height: 460px;
-  border: 1px solid #ddd;
-  background-color: #f7f7f7;
-  padding: 0 4px 0 8px;
-}
-
-.submenu-item {
-  width: 500px;
-  padding: 8px 0;
-
-  a {
-    float: left;
-    padding: 0 8px;
-    margin: 2px 0;
-    border-left: 1px solid #ccc;
-    font-size: 12px;
-    color: #666;
-    text-decoration: none;
-  }
-}
-
-.submenu-title {
-  min-width: 54px;
-  font-weight: 700;
-
-  a {
-    border: none;
-    padding: 0;
-  }
-}
-
 // 轮播图
 .center-swiper {
-  margin: 5px;
   width: 730px;
+  height: 454px;
+  margin: 5px;
+  margin-left: 215px;
   position: relative;
   z-index: 98;
+}
+
+.right-sidebar {
+  width: 250px;
+  margin-top: 5px;
+  color: #333;
+}
+
+// 快报模块
+.sidebar-report {
+  border: 1px solid #e4e4e4;
+}
+
+.side-title {
+  padding: 5px 10px;
+  margin: 5px 5px 0;
+  line-height: 22px;
+  font-size: 14px;
+  font-weight: bold;
+  border-bottom: 1px solid #e4e4e4;
+
+  a {
+    float: right;
+    color: #333;
+    font-size: 12px;
+    text-decoration: none;
+    font-weight: normal;
+  }
+}
+
+// 信息列表
+.report-list {
+  padding: 5px 15px;
+
+  a {
+    line-height: 26px;
+    height: 26px;
+    display: block;
+    font-size: 12px;
+    color: #333;
+    text-decoration: none;
+
+    & > span {
+      font-weight: bold;
+    }
+  }
+}
+
+// 服务模块
+.service-list {
+  border-right: 1px solid #e4e4e4;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+
+  & a:nth-child(1) i {
+    background-position: 0px -5px;
+  }
+  & a:nth-child(2) i {
+    background-position: -65px -5px;
+  }
+  & a:nth-child(3) i {
+    background-position: -126px -5px;
+  }
+  & a:nth-child(4) i {
+    background-position: -190px -5px;
+  }
+  & a:nth-child(5) i {
+    background-position: 0px -76px;
+  }
+  & a:nth-child(6) i {
+    background-position: -62px -76px;
+  }
+  & a:nth-child(7) i {
+    background-position: -128px -76px;
+  }
+  & a:nth-child(8) i {
+    background-position: -190px -76px;
+  }
+  & a:nth-child(9) i {
+    background-position: 0px -146px;
+  }
+  & a:nth-child(10) i {
+    background-position: -65px -146px;
+  }
+  & a:nth-child(11) i {
+    background-position: -126px -146px;
+  }
+  & a:nth-child(12) i {
+    background-position: -194px -146px;
+  }
+}
+
+.service-item {
+  width: 25%;
+  height: 64px;
+  box-sizing: border-box;
+  border-left: 1px solid #e4e4e4;
+  border-bottom: 1px solid #e4e4e4;
+  text-align: center;
+  font-size: 12px;
+
+  p {
+    line-height: 22px;
+  }
+}
+
+.service-item-icon {
+  display: block;
+  height: 40px;
+  background-image: url(./../images/icons.png);
+}
+
+// 广告图
+.sidebar-banner {
+  opacity: 0.8;
+  transition: opacity 400ms;
+  margin-top: 7px;
+  width: 250px;
+
+  &:hover {
+    opacity: 1;
+  }
 }
 </style>
