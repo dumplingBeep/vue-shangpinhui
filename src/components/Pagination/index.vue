16342741f46e983414e08cf603c572d7bf8d2159
luoxin <1813650185@qq.com>
2021-06-26 11:26:06 +0800

    分页功能

---
+++ src/components/Pagination/index.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="sui-pagination clearfix">
+    <ul>
+      <li class="prev disabled">
+        <a href="#">«上一页</a>
+      </li>
+      <li v-for="index in Math.ceil(total / pageSize)" :key="index" class="active">
+        <a href="#">{{ index }}</a>
+      </li>
+      <li v-show="Math.ceil(total / pageSize) > 5" class="dotted"><span>...</span></li>
+      <li class="next">
+        <a href="#">下一页»</a>
+      </li>
+    </ul>
+    <div>
+      <span>共{{ Math.ceil(total / pageSize) }}页&nbsp;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pagination',
+  props: {
+    total: {
+      type: Number,
+      required: true,
+    },
+    pageSize: {
+      type: Number,
+      required: true,
+    },
+  },
+  watch: {
+    total() {
+      console.log(this.total);
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.sui-pagination {
+  margin: 18px 0;
+
+  ul {
+    margin-left: 0;
+    margin-bottom: 0;
+    vertical-align: middle;
+    width: 490px;
+    float: left;
+
+    li {
+      line-height: 18px;
+      display: inline-block;
+
+      a {
+        position: relative;
+        float: left;
+        line-height: 18px;
+        text-decoration: none;
+        background-color: #fff;
+        border: 1px solid #e0e9ee;
+        margin-left: -1px;
+        font-size: 14px;
+        padding: 9px 18px;
+        color: #333;
+      }
+
+      &.active {
+        a {
+          background-color: #fff;
+          color: #e1251b;
+          border-color: #fff;
+          cursor: default;
+        }
+      }
+
+      &.prev {
+        a {
+          background-color: #fafafa;
+        }
+      }
+
+      &.disabled {
+        a {
+          color: #999;
+          cursor: default;
+        }
+      }
+
+      &.dotted {
+        span {
+          margin-left: -1px;
+          position: relative;
+          float: left;
+          line-height: 18px;
+          text-decoration: none;
+          background-color: #fff;
+          font-size: 14px;
+          border: 0;
+          padding: 9px 18px;
+          color: #333;
+        }
+      }
+
+      &.next {
+        a {
+          background-color: #fafafa;
+        }
+      }
+    }
+  }
+
+  div {
+    color: #333;
+    font-size: 14px;
+    float: right;
+    width: 241px;
+  }
+}
+</style>
